b93392ef0da50002d0bdc6e6a56d1fc00ed5d567
yosshyjungle <yosshyjungle@gmail.com>
2026-02-26 08:49:08 +0900

    2nd commit

---
+++ server.php
@@ -0,0 +1,11 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"]; 
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    echo "Name: " . $name . "<br>";
+    echo "Email: " . $email . "<br>";
+    echo "Message: " . $message;
+}
+?>
